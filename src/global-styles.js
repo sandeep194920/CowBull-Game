2f3b534a28199e6ab82c9d305218144289bd5dcc
sandeep194920 <sandeepamaranath@gmail.com>
2020-11-08 13:54:20 -0500

    first commit

---
+++ src/global-styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+html,body{
+    font-family:'Helvatica Neue', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing:antialiased;
+    -moz-font-smoothing:grayscale;
+    background-color:#14274E;
+    color: #333333;
+    font-size:16px; 
+
+}
+`;
